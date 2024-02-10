e3558b4d89ff7449aecec56a7f9531cfe4eef887
Jordan Herzstein <jordanherzstein@gmail.com>
2024-02-10 12:11:17 -0500

    auto updating site

--- build.sh
+++ build.sh
@@ -7,14 +7,6 @@ then
     git add .
     git commit -m "auto updating site"
     git push origin master
-    git switch gh-pages
-    git checkout master -- ~/Website/public/
-    mv public/* .
-    rm -r public/
-    git add .
-    git commit -m "updating for hugo"
-    git push origin gh-pages
-    git switch master
 else
     exit 0
 fi
