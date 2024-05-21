5d4dadd846e74a0ef1d0d6df13294051b327a62c
Jordan Herzstein <jordanherzstein@gmail.com>
2024-05-20 20:38:14 -0400

    auto updating site

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ hugo
 read -r -p "Do you want to sync changes to the server and update git? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-    neocities push ~/Website/public/
+    neocities push ./public/
     git add .
     git commit -m "auto updating site"
     git push origin master
